8b103f0fddee000964c98a311d2b1c260b5b2bb4
Julien <julien.renou@gmail.com>
2013-03-25 00:27:04 +0100

    add spi installation

---
+++ tmp/cache/charger_plugins_options.php
@@ -0,0 +1,9 @@
+<?php
+
+if (defined('_ECRIRE_INC_VERSION')) {
+define('_PLUGINS_HASH','cb98f4f4b46f68dc64801e7e02d5f5e8');
+include_once_check(_ROOT_PLUGINS_DIST.'petitions/petitions_options.php');
+include_once_check(_ROOT_PLUGINS_DIST.'squelettes_par_rubrique/squelettes_par_rubrique_options.php');
+include_once_check(_ROOT_PLUGINS_DIST.'compresseur/compresseur_options.php');
+}
+?>
